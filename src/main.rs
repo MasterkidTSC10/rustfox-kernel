1ec7bb84f5797c7c08d1880218ea3c108bf40f49
masterkidtsc10 <tylercrouse10@gmail.com>
2025-06-15 20:49:26 +0200

    slight bugfix

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(alloc_error_handler)]
-
+#![feature(log_syntax)]
 extern crate alloc;
 
 // module imports
@@ -10,18 +10,16 @@ mod keyboard;
 mod memory;
 mod shell;
 // import
-use core::panic::PanicInfo;
 // use graphics::fox::draw_fox;
 use core::alloc::Layout;
-use graphics::framebuffer::draw_string;
 use keyboard::{read_scancode, scancode_to_ascii};
+use limine::framebuffer::Framebuffer;
 use limine::request::FramebufferRequest;
-use shell::commands::cmd_clear;
 use shell::shell::draw_prompt;
 static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
 
 #[no_mangle]
-pub extern "C" fn _start(_multiboot_info_addr: usize) -> ! {
+pub extern "C" fn _start() -> ! {
     memory::init_heap();
     if let Some(fb_response) = FRAMEBUFFER_REQUEST.get_response() {
         if let Some(fb) = fb_response.framebuffers().next() {
@@ -47,42 +45,13 @@ pub extern "C" fn _start(_multiboot_info_addr: usize) -> ! {
 
     loop {}
 }
-static mut FB_MEM: Option<&'static mut [u8]> = None;
-static mut FB_PITCH: usize = 0;
-static mut FB_BPP: usize = 0;
-
-// Optional fallback (dummy buffer)
-static mut FALLBACK_FB: [u8; 1024] = [0; 1024]; // Small dummy buffer
-
-fn get_fb() -> (&'static mut [u8], usize, usize) {
-    unsafe {
-        let fb: &mut [u8] = match FB_MEM.as_mut() {
-            Some(inner) => *inner, // This keeps the borrow, doesn't move
-            None => &mut FALLBACK_FB,
-        };
-        let pitch = if FB_PITCH != 0 { FB_PITCH } else { 32 };
-        let bpp = if FB_BPP != 0 { FB_BPP } else { 3 };
-        (fb, pitch, bpp)
-    }
-}
 
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {
-        let (fb, pitch, bpp) = get_fb();
-        draw_string(
-            fb,
-            pitch,
-            bpp,
-            10,
-            200,
-            "!!! KERNEL PANIC !!!",
-            &[255, 0, 0],
-        );
-    }
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
 
 #[alloc_error_handler]
 fn alloc_error(_layout: Layout) -> ! {
-    panic!("allocation error");
+    panic!("allocation error!");
 }
